29c7694604dfc9f9854851f2a09720fa381aa786
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-08-13 12:57:10 +0300

    add redux

---
+++ src/redux/cart/reducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    gamesInCart: []
+  },
+  reducers: {
+    setGameInCart: (state, action) => {
+      state.gamesInCart.push(action.payload)
+    },
+    deleteGameFromCart: (state, action) => {
+    state.gamesInCart = state.gamesInCart.filter(game => game.id !== action.payload)
+    }
+  }
+})
+
+export const {setGameInCart, deleteGameFromCart} = cartSlice.actions
+export default cartSlice.reducer
