f3ec0b31c877a27f39fa7b2f48c72e3c95e4f389
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-08-13 21:30:05 +0300

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,34 @@ import { Route, Routes } from "react-router-dom";
 import s from "./App.module.css";
 import { HomePage } from "./pages/HomePages/HomePage";
 import { Header } from "./components/Header/Header";
+import { GamePage } from "./pages/GamePage/GamePage";
+import { OrderPage } from "./pages/OrderPage/OrderPage";
 
 function App() {
   return (
-    <div className={s.app}>
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={<HomePage />}
-        />
-      </Routes>
-    </div>
+    <>
+      <div className={s.header}>
+        <Header />
+      </div>
+
+      <div className={s.app}>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/games/:title"
+            element={<GamePage />}
+          />
+          <Route
+            path="/order"
+            element={<OrderPage />}
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
 export default App;
- 
